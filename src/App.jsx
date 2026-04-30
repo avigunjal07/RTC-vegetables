4d2d131f5552174d7662edd08c42434b00a7e7ec
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-01 00:18:46 +0530

    footer compopnent is created but links are not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import './index.css'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Footer />
     </div>
   )
 }
