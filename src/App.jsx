4d7fd0cfc06f0ebbc07a42ebebb90550438f0257
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-01 12:14:34 +0530

    FeaturesCard component is created and added in the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import './index.css'
-import Footer from './Components/Footer'
+// import Footer from './Components/Footer'
+import FeatureSection from './Components/FeatureCards'
 
 const App = () => {
   return (
     <div>
-      <Footer />
+      <FeatureSection />
     </div>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,50 +1,50 @@
-import { Instagram, Linkedin } from "lucide-react";
-// import {Github} from "lucide-react";
+// import { Instagram, Linkedin } from "lucide-react";
+// // import {Github} from "lucide-react";
 
 
-const SOCIAL_LINKS = [
-  {
-    name: "GitHub",
-    url: "https://github.com/avigunjal07",
-    // icon: <Github className="inline-block" />,
-  },
-  {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/avishkar-gunjal/",
-    icon: <Linkedin className="inline-block" />,
-  },
+// const SOCIAL_LINKS = [
 //   {
-//     name: "Instagram",
-//     url: "https://www.instagram.com/avi_gunjal_2507/",
-//     icon: <Instagram className="inline-block" />,
-//   }
-,
-];
+//     name: "GitHub",
+//     url: "https://github.com/avigunjal07",
+//     // icon: <Github className="inline-block" />,
+//   },
+//   {
+//     name: "LinkedIn",
+//     url: "https://www.linkedin.com/in/avishkar-gunjal/",
+//     icon: <Linkedin className="inline-block" />,
+//   },
+// //   {
+// //     name: "Instagram",
+// //     url: "https://www.instagram.com/avi_gunjal_2507/",
+// //     icon: <Instagram className="inline-block" />,
+// //   }
+// ,
+// ];
 
-const FooterLink = ({ name, url, icon }) => {
-  return (
-    <a href={url} className="mx-2 mr-3 align-middle">
-      {icon}
+// const FooterLink = ({ name, url, icon }) => {
+//   return (
+//     <a href={url} className="mx-2 mr-3 align-middle">
+//       {icon}
 
-      <span className="ml-1">{name}</span>
-    </a>
-  );
-};
+//       <span className="ml-1">{name}</span>
+//     </a>
+//   );
+// };
 
-function Footer() {
-  return (
-    <div className="w-full bg-white px-10 py-4 flex justify-around">
-      <div>
-        Owner <span style={{ color: "red" }}>Avishkar Sopan Gunjal</span> | All rights reserved 2026
-      </div>
+// function Footer() {
+//   return (
+//     <div className="w-full bg-white px-10 py-4 flex justify-around">
+//       <div>
+//         Owner <span style={{ color: "red" }}>Avishkar Sopan Gunjal</span> | All rights reserved 2026
+//       </div>
 
-      <div>
-        {SOCIAL_LINKS.map(({ name, url, icon }) => {
-          return <FooterLink key={name} name={name} url={url} icon={icon} />;
-        })}
-      </div>
-    </div>
-  );
-}
+//       <div>
+//         {SOCIAL_LINKS.map(({ name, url, icon }) => {
+//           return <FooterLink key={name} name={name} url={url} icon={icon} />;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Footer;
+// export default Footer;
